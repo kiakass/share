3d1b0cdb6cba4fdaf4f1a6956e5d1a6fe9097d2f
kiakass <junhkwak@gamil.com>
2020-09-15 17:49:20 +0900

    Add logistic_bank_exam.R

--- logistic_bank_exam.R
+++ logistic_bank_exam.R
@@ -9,7 +9,16 @@
 bank.df <- read.csv("bank.csv", 
                     header=TRUE, 
                     na.strings = ".")
+
+bank.df$Education <- factor(bank.df$Education, 
+                            levels = c(1, 2, 3), 
+                            labels = c("Undergrad", "Graduate", "Advanced/Professional"))
+
 bank.df <- bank.df[, -c(1)]
+
+
+str(bank.df)
+head(bank.df)
 
 # 02.훈련용, 검증용 데이터 분리
 set.seed(2) # 시드 고정 
@@ -38,14 +47,14 @@ round(odds, 5)
 # glm 모델에서 predict() 사용법
 # type = "response" 예측모델 확률 계산 
 logit.reg.pred <- predict(logit.reg, 
-                          valid.df[, -8], 
+                          valid.df[, -12], # Y 제거
                           type = "response")
 head(logit.reg.pred)
 
 
 # 05.실제값과 예측모델을 통해서 구한 값을 묶어서 데이터 프레임으로 저장
 # Table 10.3 실제값 중에서 5개만 화면에 보여줌
-data.frame(actual = valid.df$Personal.Loan[1:5], 
+data.frame(actual = valid.df$Y[1:5], 
            predicted = logit.reg.pred[1:5])
 
 
@@ -53,31 +62,31 @@ data.frame(actual = valid.df$Personal.Loan[1:5],
 library(caret) #모델성능평가 (ConfusionMatrx)
 pred <- predict(logit.reg, valid.df)
 confusionMatrix(as.factor(ifelse(pred > 0.5, 1, 0)), # cut off = 0.5
-                as.factor(valid.df$Personal.Loan))
+                as.factor(valid.df$Y))
 
 # 07.gain chart 만들기
 # Figure 10.3
 
 library(gains)
-gain <- gains(valid.df$Personal.Loan, 
+gain <- gains(valid.df$Y, 
               logit.reg.pred, 
               groups=10)
 
 
 # lift chart
-plot(c(0,gain$cume.pct.of.total*sum(valid.df$Personal.Loan)) ~ 
+plot(c(0,gain$cume.pct.of.total*sum(valid.df$Y)) ~ 
        c(0,gain$cume.obs),
      xlab="# cases", 
      ylab="Cumulative", 
      main="", type="l")
 
-lines(c(0,sum(valid.df$Personal.Loan)) ~ 
+lines(c(0,sum(valid.df$Y)) ~ 
         c(0, dim(valid.df)[1]), lty=2)
 
 
 
 # 10분위 차트 
-heights <- gain$mean.resp/mean(valid.df$Personal.Loan)
+heights <- gain$mean.resp/mean(valid.df$Y)
 midpoints <- barplot(heights, 
                      names.arg = gain$depth, 
                      ylim = c(0,9), 
